d018fb8c35f2680895f575b184a3173246858a77
IhabShereef <47915747+IhabShereef@users.noreply.github.com>
2023-07-24 12:09:30 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,29 +1,29 @@
 /* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+	/**
+	  ******************************************************************************
+	  * @file           : main.c
+	  * @brief          : Main program body
+	  ******************************************************************************
+	  * @attention
+	  *
+	  * Copyright (c) 2023 STMicroelectronics.
+	  * All rights reserved.
+	  *
+	  * This software is licensed under terms that can be found in the LICENSE file
+	  * in the root directory of this software component.
+	  * If no LICENSE file comes with this software, it is provided AS-IS.
+	  *
+	  ******************************************************************************
+	  */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
 #include <stdio.h>
 #include "stm32f4xx.h"
 #include <string.h>
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -58,6 +58,14 @@ static void MX_CAN1_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+CAN_TxHeaderTypeDef TxHeader;
+CAN_RxHeaderTypeDef RxHeader;
+
+uint8_t TxData[8];
+uint8_t RxData[8];
+
+
+uint32_t TxMailbox;
 
 /* USER CODE END 0 */
 
@@ -68,8 +76,9 @@ static void MX_CAN1_Init(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-char gearCombination[10];
-
+	char gearCombination[5];
+	char gearMode[5];
+	uint8_t gearIP;
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -92,57 +101,103 @@ char gearCombination[10];
   MX_GPIO_Init();
   MX_CAN1_Init();
   /* USER CODE BEGIN 2 */
+  HAL_CAN_Start(&hcan1);
 
+    HAL_CAN_ActivateNotification(&hcan1,CAN_IT_RX_FIFO0_MSG_PENDING);
+
+    TxHeader.DLC =2;
+
+    TxHeader.IDE =CAN_ID_STD;
+    TxHeader.RTR = CAN_RTR_DATA;
+    TxHeader.StdId =0x407;
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-	  GPIO_PinState rotarySwitchState0 = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_0);
-	  GPIO_PinState rotarySwitchState1 = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_1);
-	  GPIO_PinState rotarySwitchState2 = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_2);
-	  GPIO_PinState rotarySwitchState3 = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_3);
-
-	  GPIO_PinState limitSwitchState1 = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_1);
-	  GPIO_PinState limitSwitchState2 = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_2);
-	  GPIO_PinState limitSwitchState3 = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_4);
-	  
-	  if (rotarySwitchState0 == GPIO_PIN_SET){
-		  strcpy(gearCombination, "LR");
-	  }
-	  else if (rotarySwitchState1 == GPIO_PIN_SET){
-		  strcpy(gearCombination, "N");
-	  }
-	  else if (rotarySwitchState2 == GPIO_PIN_SET){
-	      gearCombination[0] = "L";
-	  }
-	  else if (rotarySwitchState3 == GPIO_PIN_SET){
-	      gearCombination[0] = "H";
-	  }
-	  else {
-	      printf("Error in Rotary switch");
-	  }
-
-	  if (gearCombination[0]=="L"|| gearCombination[0]=="H")
+	  while (1)
 	  {
-	      if (limitSwitchState1 == GPIO_PIN_SET){
-	      gearCombination[1] = "1";
-	      }
-	  else if (limitSwitchState2 == GPIO_PIN_SET){
-	      gearCombination[1] = "2";
-	  	  }
-	  else if (rotarySwitchState3 == GPIO_PIN_SET){
-	      gearCombination[1] = "3";
-	  	  }
-	  }
+    /* USER CODE END WHILE */
 
-	      
-	 
-	  
     /* USER CODE BEGIN 3 */
-  }
+		  
+		  GPIO_PinState rotarySwitchState0 = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_0);
+		  GPIO_PinState rotarySwitchState1 = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_1);
+		  GPIO_PinState rotarySwitchState2 = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_2);
+		  GPIO_PinState rotarySwitchState3 = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_3);
+
+		  GPIO_PinState limitSwitchState1 = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_1);
+		  GPIO_PinState limitSwitchState2 = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_2);
+		  GPIO_PinState limitSwitchState3 = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_4);
+		  
+		  GPIO_PinState limitSwitchStatea = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_10); //Limit switch corresponding to L
+		  GPIO_PinState limitSwitchStateb = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_11); //Limit switch corresponding to H
+		  GPIO_PinState limitSwitchStatec = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_12); //Limit switch corresponding to 1
+		  GPIO_PinState limitSwitchStated = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_13); //Limit switch corresponding to 2
+		  GPIO_PinState limitSwitchStatee = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_14); //Limit switch corresponding to 3
+		  GPIO_PinState limitSwitchStatef = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_15); //Limit switch corresponding to R
+		 	  
+		 	  if (rotarySwitchState0 == GPIO_PIN_SET){
+		 		  strcpy(gearCombination, "LR");
+		 	  }
+		 	  else if (rotarySwitchState1 == GPIO_PIN_SET){
+		 		  strcpy(gearCombination, "N");
+		 	  }
+		 	  else if (rotarySwitchState2 == GPIO_PIN_SET){
+		 	      gearCombination[0] = "L";
+		 	  }
+		 	  else if (rotarySwitchState3 == GPIO_PIN_SET){
+		 	      gearCombination[0] = "H";
+		 	  }
+		 	  else {
+		 	      printf("Error in Rotary switch");
+		 	  }
+
+		 	  if (gearCombination[0]=="L"|| gearCombination[0]=="H")
+		 	  {
+		 	      if (limitSwitchState1 == GPIO_PIN_SET){
+		 	      gearCombination[1] = "1";
+		 	      }
+		 	  else if (limitSwitchState2 == GPIO_PIN_SET){
+		 	      gearCombination[1] = "2";
+		 	  	  }
+		 	  else if (rotarySwitchState3 == GPIO_PIN_SET){
+		 	      gearCombination[1] = "3";
+		 	  	  }
+		 	  }
+		 	  if(limitSwitchStatea == GPIO_PIN_SET && limitSwitchStatec == GPIO_PIN_SET)
+		 	  {
+		 		  strcpy(gearMode, "L1");
+		 	  }
+		 	  else if(limitSwitchStatea == GPIO_PIN_SET && limitSwitchStated  == GPIO_PIN_SET)
+		 	 {
+		 		  strcpy(gearMode, "L2");
+		 	 }
+		 	 else if(limitSwitchStatea == GPIO_PIN_SET && limitSwitchStatee  == GPIO_PIN_SET)
+		 	 {
+		 	 	  strcpy(gearMode, "L3");
+		 	 }
+		 	else if(limitSwitchStateb == GPIO_PIN_SET && limitSwitchStatec  == GPIO_PIN_SET)
+		 	{
+		 		  strcpy(gearMode, "H1");
+		 	}
+		 	else if(limitSwitchStateb == GPIO_PIN_SET && limitSwitchStated  == GPIO_PIN_SET)
+		 	{
+		 		  strcpy(gearMode, "H2");
+		 	}
+		 	else if(limitSwitchStateb == GPIO_PIN_SET && limitSwitchStatee  == GPIO_PIN_SET)
+		 	{
+		 		  strcpy(gearMode, "H3");
+		 	}  
+		 	
+		 if(!strcmp(gearMode,gearCombination))
+		 {
+			 
+		 }
+		 else 
+		 {
+			printf("Error!"); 
+		 }
+	  }
   /* USER CODE END 3 */
 }
 
@@ -238,8 +293,8 @@ static void MX_GPIO_Init(void)
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pins : PB1 PB2 PB3 */
   GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3;
@@ -247,8 +302,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PD0 PD1 PD2 PD3 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3;
+  /*Configure GPIO pins : PD10 PD11 PD12 PD13
+                           PD14 PD15 PD0 PD1
+                           PD2 PD3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_13
+                          |GPIO_PIN_14|GPIO_PIN_15|GPIO_PIN_0|GPIO_PIN_1
+                          |GPIO_PIN_2|GPIO_PIN_3;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
@@ -268,11 +327,11 @@ static void MX_GPIO_Init(void)
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
+	  /* User can add his own implementation to report the HAL error return state */
+	  __disable_irq();
+	  while (1)
+	  {
+	  }
   /* USER CODE END Error_Handler_Debug */
 }
 
@@ -287,8 +346,8 @@ void Error_Handler(void)
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+	  /* User can add his own implementation to report the file name and line number,
+		 ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
